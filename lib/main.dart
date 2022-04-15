848749bcd49e80fddb76e1544e014a8bd9b60130
Kyle Ryu <kyleryu6698@gmail.com>
2022-04-15 23:45:09 +0900

    ListView.builder, FloatingActionButton Update

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,32 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
 
+    var a = 1;
+
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: () {
+            a++;
+          },
+        ),
         appBar: AppBar(
           title: Text('Test App')
         ),
-        body: ListView(
-          children: [
-            ListTile(
+        body: ListView.builder(
+          itemCount: 15,
+          itemBuilder: (context, i) {
+            return ListTile(
               leading: Icon(Icons.contact_page),
               title: Text('Daniel Ek'),
-            ),
-          ],
+            );
+          },
         ),
         bottomNavigationBar: BtmNav()
         ),
